ca2ee6a8842ee69330602753957af4af38ac8f1e
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-05-25 22:24:35 -0400

    Create own prop item

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import data from "./data.js"
 import './App.css';
 
 export default function App() {
-  const cards = data.map(item => {
+  const cards = data.map(obj => {
     return (
       <Card 
-          key = {item.id}
-          img={item.coverImg}
-          rating={item.stats.rating}
-          reviewCount={item.stats.reviewCount}
-          location={item.location}
-          title={item.title}
-          price={item.price}
-          openSpots={item.openSpots}
+          key = {obj.id}
+          // create own prop called item
+          item = {obj}
+              // {item: {id: 1, 
+              // title: "Life Lessons with Katie Zaferes", 
+              // description: "I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.", 
+              // price: 136, coverImg: "katie-zaferes.png", 
+              // stats: {rating: 5, reviewCount: 6}, 
+              // location: "Online", 
+              // openSpots: 0}}
       />
     )
 })        
@@ -24,9 +26,9 @@ export default function App() {
 return (
   <div>
     <Navbar />
-      <section className="cards-list">
-          {cards}
-      </section>
+    <section className="cards-list">
+        {cards}
+    </section>
   </div>
 )
 }
